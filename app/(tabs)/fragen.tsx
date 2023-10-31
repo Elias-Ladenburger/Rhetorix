6c271653c0d661df7535d41480d4df10ecdcc33f
Elias Ladenburger <elias@ladenburger-family.de>
2023-10-31 22:34:01 +0100

    Start building question page

--- app/(tabs)/fragen.tsx
+++ app/(tabs)/fragen.tsx
@@ -1,19 +1,33 @@
-import { StyleSheet } from 'react-native';
 
-import RhetoricsQuiz from '../../components/RhetoricsQuiz';
+import {SafeAreaView, StyleSheet, TextInput} from 'react-native';
 import { Text, View } from '../../components/Themed';
+import React from 'react';
 
 export default function TabTwoScreen() {
+  const [text, onChangeText] = React.useState('');
+  const [number, onChangeNumber] = React.useState('');
   return (
+
     <View style={styles.container}>
-      <Text style={styles.title}>Stelle deine Frage</Text>
+      <Text style={styles.title}>Was für eine Frage treibt dich um?</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <RhetoricsQuiz></RhetoricsQuiz>
+      <SafeAreaView style={styles.safe}>
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeText}
+        value={text}
+        multiline = {true}
+        numberOfLines = {4}
+      />
+    </SafeAreaView>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  safe: {
+    flex: 1
+  },
   container: {
     flex: 1,
     alignItems: 'center',
@@ -27,5 +41,11 @@ const styles = StyleSheet.create({
     marginVertical: 30,
     height: 1,
     width: '80%',
+  },
+  input: {
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    backgroundColor: "#FFF",
   },
 });
--- documentation/concept.md
+++ documentation/concept.md
@@ -7,3 +7,7 @@ Possible incarnations are:
 - a quiz about rhetorics and public speaking.
 - a learning app using gamification.
 - a survey app which gives consumers coupons in exchange for filling out customer surveys
+
+
+For now, this app is developed as a forum for the ambitious professional community in Germany.
+First target will be (student) and professional consultants in the DACH region, giving each other career advice.
