86a27729794d6c7715cb75f6ec6d797e8b2f92a0
Elias Ladenburger <elias@ladenburger-family.de>
2023-11-01 22:42:06 +0100

    Implement working input form

--- app/(tabs)/fragen.tsx
+++ app/(tabs)/fragen.tsx
@@ -1,25 +1,34 @@
 
-import {SafeAreaView, StyleSheet, TextInput} from 'react-native';
+import {Button, Keyboard, Pressable, SafeAreaView, StyleSheet, TextInput, TouchableOpacity} from 'react-native';
 import { Text, View } from '../../components/Themed';
 import React from 'react';
 
 export default function TabTwoScreen() {
   const [text, onChangeText] = React.useState('');
-  const [number, onChangeNumber] = React.useState('');
+  const [submittedQuestion, onChangeSubmit] = React.useState('');
+
+
   return (
 
     <View style={styles.container}>
+            <Pressable onPress={Keyboard.dismiss}>
       <Text style={styles.title}>Was für eine Frage treibt dich um?</Text>
+
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <SafeAreaView style={styles.safe}>
-      <TextInput
-        style={styles.input}
-        onChangeText={onChangeText}
-        value={text}
-        multiline = {true}
-        numberOfLines = {4}
-      />
-    </SafeAreaView>
+
+        <SafeAreaView style={styles.safe}>
+          <TextInput
+            style={styles.input}
+            onChangeText={onChangeText}
+            value={text}
+            multiline = {true}
+            numberOfLines = {4}
+            onSubmitEditing={Keyboard.dismiss}
+          />
+          <Button title={"Submit"} color={"#eee"} onPress={()=>onChangeSubmit(text)}></Button>
+      </SafeAreaView>
+            <Text style={styles.title}>{submittedQuestion}</Text>
+      </Pressable>
     </View>
   );
 }
